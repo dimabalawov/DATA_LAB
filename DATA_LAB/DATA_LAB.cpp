db0a9d7e70ceed677a756d6dd4ff1645c678e1fc
dimabalawov <dimabalawov@gmail.com>
2023-09-17 21:30:59 +0300

    FINAl

--- DATA_LAB/DATA_LAB.cpp
+++ DATA_LAB/DATA_LAB.cpp
@@ -6,9 +6,28 @@ using namespace std;
 int main()
 {
     Data a;
+    Data b(4,5,2004);
     a.Input();
     a.Print();
-    a-=10;
+    a++;
     a.Print();
+    a--;
+    a.Print();
+    a += 4;
+    a.Print();
+    a -= 4;
+    a.Print();
+    a = a + 4;
+    a.Print();
+    a = a - 4;
+    a.Print();
+    a = a - b;
+    a.Print();
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
 }
 
